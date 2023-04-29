883ccfd7b4e1cbbbdb3309b2d7abab9e938f6dbe
Stefan Hellings <hellingsstefan@gmail.com>
2023-04-29 19:35:24 +0200

    Add theme provider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
